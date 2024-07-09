e8b45b60aaec0536aff13a7362e2267ef1a6ed53
karsh <utkarsh@cyberfear.com>
2024-07-10 01:36:15 +1000

    config file
    + added config file creation, ensuring its path exists
    + added default actions, should config miss something
    + created new ensure_exists file, to share logic between file and config

--- cmd/config.go
+++ cmd/config.go
@@ -1,28 +1,47 @@
 
 /* config.go
- * configure how the utility behaves
- */
+* configure how the utility behaves
+*/
 
 package cmd
 
 import (
-    "os"
+    "github.com/BurntSushi/toml"
 )
 
 //-- local ---------------------------------------------------------------------
 
-func config_exists() bool {
-    return false
+func default_properties(aProperty *string) string {
+    switch *aProperty {
+        case "local": return "false"  // by default dont save todo in local dir
+        case "ignore": return "false"  // by default dont ignore new todo files
+        case "location": return "Documents/Todo" // location for all todo files
+    }
+
+    return ""
+}
+
+func parse_toml(aProperty *string) string {
+    switch *aProperty {
+    }
+
+    return ""
 }
 
 
 //-- global --------------------------------------------------------------------
 
 func Config_Init() {
-    //
+    ensure_exists_dir("/.config")
+    ensure_exists_file("/.config/todogo.toml")
 }
 
-func Config_Open() {
-    //
+func Config_Read(aProperty *string) string {
+    lConfig := parse_toml(aProperty)
+    if lConfig == "" {
+        return default_properties(aProperty)
+    }
+
+    return lConfig
 }
 
---
+++ cmd/ensure_exists.go
@@ -0,0 +1,41 @@
+
+/* ensure_exists.go
+ * ensures if certain files or directories exists
+ */
+
+package cmd
+
+import(
+    "os"
+    "fmt"
+    "errors"
+)
+
+func ensure_exists_dir(aPath string) {
+    home, _:= os.UserHomeDir()
+    lPath  := home + aPath
+
+    _, aPath_exists := os.Stat(lPath)
+    if errors.Is(aPath_exists, os.ErrNotExist) {
+        create_dir_err := os.MkdirAll(lPath, 0770)
+
+        if create_dir_err != nil {
+            fmt.Fprintf(os.Stderr, "Error: %v", create_dir_err)
+        }
+    }
+}
+
+func ensure_exists_file(aFile string) {
+    home, _:= os.UserHomeDir()
+    lFile := home + aFile
+
+    _, config_file_exists := os.Stat(lFile)
+    if errors.Is(config_file_exists, os.ErrNotExist) {
+        _, create_file_err := os.Create(lFile)
+
+        if create_file_err != nil {
+            fmt.Fprintf(os.Stderr, "Error: %v", create_file_err)
+        }
+    }
+}
+
